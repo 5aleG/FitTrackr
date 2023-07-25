9969e8bd32c0959f7eb819ed6bc3420fbee4bb2b
5aleG <sascha@digicy.ch>
2023-07-25 17:55:20 +0200

    feat: Add animated DropUpMenu with input forms

--- frontend/src/Components/Navbar/DropUpMenu/dropUpMenuStyled.js
+++ frontend/src/Components/Navbar/DropUpMenu/dropUpMenuStyled.js
@@ -10,10 +10,11 @@ export const DropUpContainer = styled.div`
     align-items: center;
     justify-content: center;
     width: 95%;
-    height: 100px;
-    background-color: rgba(87, 88, 153, 0.95); 
+    min-height: 100px;
+    background-color: #575899;
     border-radius: 40px;
     padding: 10px;
+    ${(props) => props.expanded && 'max-height: 300px;'} 
 `;
 
 export const MenuItem = styled.div`
@@ -33,6 +34,41 @@ export const CircleIcon = styled.div`
     width: 50px;
     height: 50px;
     border-radius: 50%;
-    background-color: ${props => props.color};
+    background-color: ${(props) => props.color};
     margin-bottom: 8px;
 `;
+
+export const FormField = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    margin-top: 5px;
+    margin-bottom: 10px;
+
+    input[type="number"] {
+        width: 120px;
+        background-color: #6F70C0;
+        color: #fff;
+        padding: 10px;
+        border: 1px solid #8E8FF5;
+        border-radius: 4px;
+
+        
+        &::placeholder {
+        color: #C0C1F8;
+        }
+    }
+        button {
+            color: #fff;
+            border: none;
+            padding: 8px 10px;
+            border-radius: 10px;
+            cursor: pointer;
+
+            &:last-child {
+            background-color: #28A77B;
+            margin-left: 10px;
+            }
+        }
+    `;
