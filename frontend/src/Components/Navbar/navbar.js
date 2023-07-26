d7189991e73bd73375398744675e073fee637a2e
5aleG <sascha@digicy.ch>
2023-07-26 13:12:33 +0200

    feat: Reduce size of plus icon in navbar

--- frontend/src/Components/Navbar/navbar.js
+++ frontend/src/Components/Navbar/navbar.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { IoMdHome, IoMdApps, IoIosStats, IoMdPerson } from 'react-icons/io';
 import { NavbarContainer, IconWrapper, Icon, PlusIcon } from './navbarStyled';
 import DropUpMenu from './DropUpMenu/dropUpMenu';
 
 const Navbar = () => {
+  const location = useLocation(); 
   const [isHidden, setIsHidden] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
@@ -39,7 +41,7 @@ const Navbar = () => {
   return (
     <NavbarContainer isHidden={isHidden}>
       <IconWrapper>
-        <Icon>
+        <Icon isActive={location.pathname === '/home'}>
           <IoMdHome size={25} />
         </Icon>
         <Icon>
